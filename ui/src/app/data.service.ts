12630d60c431940de10fad1f2fc14b62b2e06ffe
Jay Watson <jaywatson1982@gmail.com>
2024-03-19 05:42:05 -0500

    Update build spec and ui service file

--- ui/src/app/data.service.ts
+++ ui/src/app/data.service.ts
@@ -10,95 +10,83 @@ declare const STAGE: string;
   providedIn: 'root'
 })
 export class DataService {
-    options;
-    apiRoot: string = environment.apiUrl;
-    constructor(private httpClient: HttpClient) {}
+  options;
+  apiRoot: string = environment.apiUrl;
+  constructor(private httpClient: HttpClient) { }
 
-    setOptions() {
-        this.options = {
-            headers: {
-                app_user_id: '12345',
-                app_user_name: 'fred'
-            }
-        };
-    }
-
-    addNote(item) {
-        //let path = STAGE + '/note';
-        let endpoint = 'http://localhost:3000/dev/note'; //API_ROOT + path;
-
-        let itemData;
-        itemData = {
-            content: item.content,
-            cat: item.cat
-        };
-
-        if(item.title != "") {
-            itemData.title = item.title;
-        }
-
-        let reqBody = {
-            Item: itemData
-        };
-        this.setOptions();
-        return this.httpClient.post(endpoint, reqBody, this.options);
-    }
-
-    updateNote(item) {
-        //let path = STAGE + '/note';
-        console.log('update note', item)
-        let endpoint = 'http://localhost:3000/dev/note'; //API_ROOT + path;
-
-        let itemData;
-        itemData = {
-            content: item.content,
-            cat: item.cat,
-            timestamp: parseInt(item.timestamp),
-            note_id: item.note_id
-        };
-
-        if (item.title != "") {
-            itemData.title = item.title;
-        }
-
-        let reqBody = {
-            Item: itemData
-        };
-        this.setOptions();
-        console.log('req', reqBody)
-        return this.httpClient.patch(endpoint, reqBody, this.options);
-    }
-
-    deleteNote(timestamp) {
-        let path = '/dev/note/t/' + timestamp;
-        let endpoint = 'http://localhost:3000' + path;
-        this.setOptions();
-        return this.httpClient.delete(endpoint, this.options);
-    }
-
-    getNote(guid: string): Observable<any> {
-      // let path = STAGE + '/notes?limit=8';
-
-      // if (start > 0) {
-      //     path += '&start=' + start;
-      // }
-      // let endpoint = API_ROOT + path;
-      const endpoint = `http://localhost:3000/dev/note/n/${guid}`;
-      this.setOptions();
-      return this.httpClient.get(endpoint, this.options);
+  setOptions() {
+    this.options = {
+      headers: {
+        app_user_id: '12345',
+        app_user_name: 'fred'
+      }
+    };
   }
 
-    getNotes(start?): Observable<any> {
-        console.log('api', this.apiRoot)
-        // let path = STAGE + '/notes?limit=8';
-        let path = `${this.apiRoot}/notes?limit=5`;
+  addNote(item) {
+    let endpoint = `${this.apiRoot}/note`
 
-        if (start > 0) {
-            path += '&start=' + start;
-        }
-        // let endpoint = API_ROOT + path;
-        const endpoint = path;
-        this.setOptions();
-        return this.httpClient.get(endpoint, this.options);
+    let itemData;
+    itemData = {
+      content: item.content,
+      cat: item.cat
+    };
+
+    if (item.title != "") {
+      itemData.title = item.title;
     }
+
+    let reqBody = {
+      Item: itemData
+    };
+    this.setOptions();
+    return this.httpClient.post(endpoint, reqBody, this.options);
+  }
+
+  updateNote(item) {
+    let endpoint = `${this.apiRoot}/note`
+
+    let itemData;
+    itemData = {
+      content: item.content,
+      cat: item.cat,
+      timestamp: parseInt(item.timestamp),
+      note_id: item.note_id
+    };
+
+    if (item.title != "") {
+      itemData.title = item.title;
+    }
+
+    let reqBody = {
+      Item: itemData
+    };
+    this.setOptions();
+
+    return this.httpClient.patch(endpoint, reqBody, this.options);
+  }
+
+  deleteNote(timestamp) {
+    let endpoint = `${this.apiRoot}/note/t/${timestamp}}`;
+    this.setOptions();
+    return this.httpClient.delete(endpoint, this.options);
+  }
+
+  getNote(guid: string): Observable<any> {
+    const endpoint = `${this.apiRoot}/note/n/${guid}`;
+    this.setOptions();
+    return this.httpClient.get(endpoint, this.options);
+  }
+
+  getNotes(start?): Observable<any> {
+    console.log('api', this.apiRoot);
+    let endpoint = `${this.apiRoot}/notes?limit=5`;
+
+    if (start > 0) {
+      endpoint += '&start=' + start;
+    }
+
+    this.setOptions();
+    return this.httpClient.get(endpoint, this.options);
+  }
 }
