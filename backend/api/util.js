1dba20e38c7c39960b8b89f2ffeb0366ac80fd28
Jay Watson <jaywatson1982@gmail.com>
2024-03-26 05:49:05 -0500

    Add authenticator

--- backend/api/util.js
+++ backend/api/util.js
@@ -2,18 +2,23 @@ const getUserId = (headers) => {
   return headers.app_user_id;
 }
 
-const getUsername = (headers) => {
-  return headers.app_username;
+const getUserName = (headers) => {
+  return headers.app_user_name;
+}
+
+const getIdToken = (headers) => {
+  return headers.Authorization;
 }
 
 const getResponseHeaders = () => {
   return {
-    'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*'
   }
 }
 
 module.exports = {
   getUserId,
-  getUsername,
+  getUserName,
+  getIdToken,
   getResponseHeaders
 }
